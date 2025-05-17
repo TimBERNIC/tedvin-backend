18ec386ceabeb7cf36bbeccd8f7c3642cf8fa2d6
Tim BERNIC <nicolet.t@hotmail.com>
2025-05-17 16:31:40 +0200

    bug fix

--- routes/User.js
+++ routes/User.js
@@ -104,7 +104,7 @@ router.delete("/user/delete/:id", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
     await User.findByIdAndDelete(req.params.id);
-    await cloudinary.uploader.api.delete_folder(`Tinved/user`);
+    await cloudinary.api.delete_folder(`Home/Tinved/user/${req.params.id}`);
     return res.status(200).json({ message: "User delete success" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
